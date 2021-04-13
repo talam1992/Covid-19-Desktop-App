72efe5ddaa45ecea27ae241bc1a0d7398914c9c3
tal1992 <timothylam92@hotmail.co.uk>
2021-04-13 11:39:59 +0100

    Setting up our App Files and Background

---
+++ main.py
@@ -0,0 +1,34 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
+from kivy.core.text import LabelBase
+from kivy.uix.button import ButtonBehavior,Button
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager,Screen,NoTransition,CardTransition
+from kivy.config import Config
+
+
+
+Config.set('graphics', 'width', '1200')
+Config.set('graphics', 'height', '900')
+Config.set('graphics', 'borderless', '1')
+Config.write()
+
+
+class DashboardScreen(Screen):
+    pass
+
+GUI = Builder.load_file("main.kv")
+
+class MainApp(App):
+    def build(self):
+        return GUI
+
+    def on_start(self):
+        pass
+
+
+MainApp().run()
+
